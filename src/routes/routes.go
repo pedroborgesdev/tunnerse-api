6df1059aadfc580cc1c31510fb5dd635aa242c36
pedroborgesdev <pedrofilipe.github@gmail.com>
2025-07-03 11:01:57 -0700

    first commit

---
+++ src/routes/routes.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"net/http"
+	"tunnerse/config"
+	"tunnerse/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(router *gin.Engine) {
+
+	tunnelController := controllers.NewTunnelController()
+
+	router.GET("/health", func(c *gin.Context) {
+		c.String(http.StatusOK, "OK")
+	})
+
+	tunnel := router.Group("/")
+
+	if config.AppConfig.SUBDOMAIN {
+		tunnel.POST("/register", tunnelController.Register)
+		tunnel.GET("/tunnel", tunnelController.Get)
+		tunnel.POST("/response", tunnelController.Response)
+		tunnel.POST("/close", tunnelController.Close)
+		tunnel.GET("/", tunnelController.Tunnel)
+		tunnel.HEAD("/", tunnelController.Tunnel)
+
+		router.NoRoute(tunnelController.Tunnel)
+	}
+
+	if !config.AppConfig.SUBDOMAIN {
+		tunnel.POST("/register", tunnelController.Register)
+		tunnel.GET(":name/tunnel", tunnelController.Get)
+		tunnel.POST(":name/response", tunnelController.Response)
+		tunnel.POST(":name/close", tunnelController.Close)
+		tunnel.GET(":name/", tunnelController.Tunnel)
+		tunnel.HEAD(":name/", tunnelController.Tunnel)
+
+		router.NoRoute(tunnelController.Tunnel)
+	}
+}
